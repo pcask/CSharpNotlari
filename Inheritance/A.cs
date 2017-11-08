5c147696779a0b9b17b2fce46e8f44ba9af54f4a
sezer.bem <sezer.bem@outlook.com>
2017-11-08 13:52:32 +0300

    08.11.2017

---
+++ Inheritance/A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class A
+    {
+        public int Yukseklik { get; set; }
+        public int Genislik { get; set; }
+
+        public void Deneme()
+        {
+            Console.WriteLine("A sınıfının metodu çalışıyor");
+        }
+    }
+}
