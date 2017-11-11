d91d1cd9ef2425f1dae787aa6ebe8371b8455a27
Sezer Ayran <sezer.ayran@outlook.com>
2017-11-11 12:54:21 +0200

    sayiCozumleme

---
+++ Ornekler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region SayiCozumleme
+
+            string gelenDeger = "";
+
+            do
+            {
+                Console.Write("\nLütfen bir sayı giriniz: ");
+                try
+                {
+                    gelenDeger = Console.ReadLine();
+                    int sayi = int.Parse(gelenDeger);
+                    Console.WriteLine("***********************");
+
+                    int basamak = 1;
+                    while (sayi > 0)
+                    {
+                        Console.WriteLine(sayi % 10 + " x " + basamak + " = " + (sayi % 10) * basamak);
+
+                        basamak *= 10;
+                        sayi /= 10;
+                    }
+                }
+                catch (Exception)
+                {
+                    if (gelenDeger != "exit")
+                    {
+                        Console.WriteLine("Doğru formatta bir sayı girmediniz!");
+                        Console.Write("***********************\n");
+                    }
+                }
+
+            } while (gelenDeger != "exit");
+
+            #endregion
+
+
+        }
+    }
+}
